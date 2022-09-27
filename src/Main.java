4fe23d1ea3e0a6cee55ca3e7a59f3c3b747fffcf
EaBack <113344913+EaBack@users.noreply.github.com>
2022-09-27 09:50:24 +0200

    first commit, new files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        //build a playing field
+
+        // first make sure we can have a grid and print it to the terminal
+        //So build array to hold char, because we are going to play with x + o char
+        // need three lists or a x,y grid to place and save the chars
+        //probably going to have to be a separate class so at least 1 class will be be board
+
+
+    }
+}
